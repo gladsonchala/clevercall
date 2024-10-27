f4a322a580f382c25e929539b3be74ada19c37ed
gladsonchala <gladsonchala@gmail.com>
2024-10-28 00:28:51 +0300

    feat: taking user input cli

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from config import account_id, api_token, url
 from tools.tool_manager import ToolManager
 
 def main():
-    user_query = "What is the current price of AAPL?"
+    user_query = input("Enter your question: ")
     tool_manager = ToolManager(account_id, api_token)
     tool_manager.process_query(user_query)
 
